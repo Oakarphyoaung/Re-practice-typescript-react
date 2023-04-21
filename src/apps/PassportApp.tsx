02b6095315859e4889a070cad41f2c944a1ac93a
Oakar <linn.998.mlm@gmail.com>
2023-04-21 18:58:41 +0630

    modified passportApp

--- src/apps/PassportApp.tsx
+++ src/apps/PassportApp.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Box, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 const PassportApp = () => {
-  return <div>Passport App</div>;
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    console.log("inside UseEffect");
+  }, []);
+
+  console.log("outside UseEffect");
+
+  return (
+    <Box
+      sx={{
+        mw: "300px",
+        display: "flex",
+        justifyContent: "center",
+        p: 2,
+        m: 3,
+      }}
+    >
+      <h1>Passport</h1>
+      <Button
+        variant="contained"
+        onClick={() => {
+          setNumber(number + 1);
+        }}
+      >
+        Click{number}
+      </Button>
+    </Box>
+  );
 };
 
 export default PassportApp;
